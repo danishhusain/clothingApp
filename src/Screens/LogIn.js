bd9b0a40a13d9efc91e62375087bb32058b96ab4
Danish Hussain <danishhussain2000@gmail.com>
2022-12-29 13:33:30 +0530

    GoogleSign Done

--- src/Screens/LogIn.js
+++ src/Screens/LogIn.js
@@ -5,7 +5,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import Loader from '../Common/Loader'
 import Firebase from '../FireBase/Firebase'
 
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { GoogleSignin,statusCodes } from '@react-native-google-signin/google-signin';
 import auth from '@react-native-firebase/auth';
 import { useNavigation } from '@react-navigation/native';
 
@@ -18,26 +18,59 @@ const LogIn = ({ navigation }) => {
   const [error, setError] = useState(false)
 
 
-  //googleSignIn
+  //UseEffect Google SignIn
   useEffect(() => {
     GoogleSignin.configure({
       webClientId: '1095484900051-gk2nijhng0cvs6vuurbb011he0ns0ljm.apps.googleusercontent.com',
     });
   }, [])
 
+  // // Google SignIn
+  // const googleSihnIn = async () => {
+  //   // Check if your device supports Google Play
+  //   await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+  //   // Get the users ID token
+  //   // const { idToken } = await GoogleSignin.signIn();
+  //   const { idToken } = await GoogleSignin.signIn();
+
+  //   // Create a Google credential with the token
+  //   const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+  //   // Sign-in the user with the credential
+  //   return auth().signInWithCredential(googleCredential);
+  // }
+
+
+  // Somewhere in your code
   const googleSihnIn = async () => {
-    // Check if your device supports Google Play
-    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
-    // Get the users ID token
-    const { idToken } = await GoogleSignin.signIn();
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      // this.setState({ userInfo });
+      console.log("userInfo", userInfo)
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.log(error)
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.log(error)
 
-    // Create a Google credential with the token
-    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.log(error)
 
-    // Sign-in the user with the credential
-    return auth().signInWithCredential(googleCredential);
-  }
+        // play services not available or outdated
+      } else {
+        console.log(error)
 
+        // some other error happened
+      }
+    }
+  };
+
+
+
+  // validation Email & passward
   const Validation_Login = () => {
     setModalVisible(true)
     if (email.length == 0) {
@@ -59,7 +92,7 @@ const LogIn = ({ navigation }) => {
     }
 
   }
-
+  //get Data from async storage
   const getData = async () => {
     const gEmail = await AsyncStorage.getItem("Email")
     const gPass = await AsyncStorage.getItem("Passward")
@@ -75,12 +108,10 @@ const LogIn = ({ navigation }) => {
       setError(true)
     }
   }
-
-
   return (
     <View style={{ flex: 1, justifyContent: 'center' }}>
       <Text style={{ alignSelf: 'center', fontSize: 25, fontWeight: '600' }}>LogIn</Text>
-      <View style={{ marginHorizontal: 10, justifyContent: 'space-between',marginBottom:10 }}>
+      <View style={{ marginHorizontal: 10, justifyContent: 'space-between', marginBottom: 10 }}>
         <TextInput value={email} style={{ marginVertical: 5 }} mode='outlined' label={"Email"} left={<TextInput.Icon icon={"email"} />}
           onChangeText={(txt) => setEmail(txt)} />
         {
@@ -89,14 +120,15 @@ const LogIn = ({ navigation }) => {
         <TextInput value={passward} style={{ marginVertical: 5 }} mode='outlined' label={"Passward"} secureTextEntry left={<TextInput.Icon icon={"key"} />}
           onChangeText={(txt) => setPassward(txt)} />
         {badpassward && (<Text style={{ color: 'red' }}> Enter passward</Text>)}
-        {error && <Text style={{ color: 'red', margin: 3 }}>Email or Passward does not match</Text>}
+        {error && <Text style={{ color: 'red', margin: 3 }}>Email or Passward  not matched</Text>}
       </View>
 
       <View style={{ alignContent: 'center', justifyContent: 'center', }}>
         <Button mode='contained' onPress={() => Validation_Login()} style={{ marginBottom: 10 }}>LogIn</Button>
         <Button mode='contained' onPress={() => {
-          googleSihnIn().then(res => { console.log("res", res) })
-            .catch(error => { console.log(">>", error) })
+          googleSihnIn()
+            // .then(res => { console.log("res", res) })
+            // .catch(error => { console.log(">>", error) })
         }
         }>SignIn in with Google</Button>
         <Text style={{ fontSize: 25, fontWeight: '400', alignSelf: 'center', textDecorationLine: 'underline' }}
