d148124e4685011e569b1bd72ea3081c9ae61db2
Danish Hussain <danishhussain2000@gmail.com>
2023-01-06 12:25:58 +0530

    code clean 2

--- App.js
+++ App.js
@@ -1,91 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import AppNavigator from './src/AppNavigator'
-import LogIn from './src/Screens/LogIn'
-import SplashScreen from './src/Screens/SplashScreen'
-import Loader from './src/Common/Loader'
-import HomeScreen from './src/Screens/HomeScreen'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import Profile from './src/Bottom/Profile'
-import CartContextProvider from './src/Context/CartContext'
-import MyAddress from './src/Screens/MyAddress'
-import Addaddress from './src/Screens/AddAddress'
-import { StatusBar } from 'react-native'
-import { AuthContext, AuthProvider } from './src/Context/AuthContext'
-import AuthNavigator from './src/Screens/AuthNavigator'
-import { useContext } from 'react'
-import Navigation from './src/Navigation'
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import CartContextProvider from './src/Context/CartContext';
+import {AuthContext, AuthProvider} from './src/Context/AuthContext';
 
+import Navigation from './src/Navigation';
 
 const App = () => {
-  // const {userToken}=useContext(AuthContext)
-  // console.log("kk",userToken)
   return (
-
     <CartContextProvider>
       <AuthProvider>
-        {/* <HomeScreen /> */}
-        {/* <LogIn/> */}
-        {/* <AppNavigator/> */}
         <Navigation />
-        {/* <AuthNavigator/> */}
-        {/* <Profile/> */}
-        {/* <MyAddress/> */}
-        {/* <Addaddress/> */}
       </AuthProvider>
     </CartContextProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
-
-
-
-// import { StyleSheet, Text, View } from 'react-native'
-// import React, { useEffect, useState } from 'react'
-// import AppNavigator from './src/AppNavigator'
-// import LogIn from './src/Screens/LogIn'
-// import SplashScreen from './src/Screens/SplashScreen'
-// import Loader from './src/Common/Loader'
-// import HomeScreen from './src/Screens/HomeScreen'
-// import AsyncStorage from '@react-native-async-storage/async-storage'
-// import Profile from './src/Bottom/Profile'
-// import CartContextProvider from './src/Context/CartContext'
-// import MyAddress from './src/Screens/MyAddress'
-// import Addaddress from './src/Screens/AddAddress'
-// import { StatusBar } from 'react-native'
-// import {  AuthContext, AuthProvider } from './src/Context/AuthContext'
-// import AuthNavigator from './src/Screens/AuthNavigator'
-// import { useContext } from 'react'
-
-
-
-
-
-
-// const App = () => {
-//   // const { userToken } = useContext(AuthContext)
-//   const {userToken,setusertToken}=useContext(AuthContext)
-//   // const[d,setd]=useState('jkj')
-// console.log("token",userToken)
-//   return (
-//     <AuthProvider>
-
-//     <CartContextProvider>
-
-//         {
-//           userToken == null ? <><AuthNavigator /></> :
-//             <> <AppNavigator /></>
-//         }
-
-//     </CartContextProvider>
-//     </AuthProvider>
-
-//   )
-// }
-
-// export default App
-
-// const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,22 +1,16 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React, {useEffect, useState} from 'react';
-import {useContext} from 'react';
 import AppNavigator from './AppNavigator';
 import AuthNavigator from './Screens/AuthNavigator';
-import {AuthContext} from './Context/AuthContext';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Loader from './Common/Loader';
 import auth, {firebase} from '@react-native-firebase/auth';
 
-const Stack = createNativeStackNavigator();
 
+const Stack = createNativeStackNavigator();
 const Navigation = () => {
-  const {userToken, setUserToken, isLoggedIn, isLoading,} =
-    useContext(AuthContext);
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState();
-  // const [user, setUser] = useState();
 
   // Handle user state changes
   const onAuthStateChanged = user => {
@@ -24,38 +18,21 @@ const Navigation = () => {
     if (initializing) setInitializing(false);
   };
 
+  //useEffect for Handle user state changes
   useEffect(() => {
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-
     return subscriber; // unsubscribe on unmount
   }, []);
 
+  //
   if (initializing) return null;
 
-  //getCurrentUserInfo
-  const getCurrentUserInfo = async () => {
-    try {
-      const user = await GoogleSignin.signInSilently();
-      // this.setState({ userInfo });
-      console.log('getCurrentUserInfo', user);
-    } catch (error) {
-      if (error.code === statusCodes.SIGN_IN_REQUIRED) {
-        // user has not signed in yet
-        console.log('user has not signed in yet');
-      } else {
-        // some other error
-      }
-    }
-    return setUser(user);
-  };
   return (
     <NavigationContainer>
-      {/* {userToken !== null ? <AppNavigator /> : <AuthNavigator />} */}
       {user ? <AppNavigator /> : <AuthNavigator />}
     </NavigationContainer>
   );
 };
 
 export default Navigation;
-
 const styles = StyleSheet.create({});
