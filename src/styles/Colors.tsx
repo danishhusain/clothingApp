c6ee3b3ee1b602127c7d3adcbc6829c9db3d0515
danishrazik <danishhusain2000@gmail.com>
2023-03-10 00:32:37 +0530

    check

--- src/styles/Colors.tsx
+++ src/styles/Colors.tsx
@@ -1,24 +1,3 @@
-// export const Colors = {
-//     RED: '#E50914',
-//     ORANGE: '#DC7633',
-//     PURPLE: '#8E44AD',
-//     GRAY: '#566573',
-//     GREEN: "green",
-//     WHITE: '#FFFFFF',
-//     BLACK: '#000000',
-//     BG: '#191919',
-//     CARDCOLOR: '#0080ff',
-//     appBG: '#564d4d',
-//     backgroundColor: 'rgba(52, 52, 52, 0.8)'
-
-
-// // }
-// import { useColorScheme } from 'react-native';
-// export const colorScheme = useColorScheme()
-// const theme = colorScheme === 'dark' ? lightColors : darkColors
-
-// import { Appearance } from 'react-native';
-
 export const lightColors = {
   primary: '#6a5acd',
   // primary: '#007AFF',
@@ -39,4 +18,3 @@ export const darkColors = {
 
 };
 
-// export const colors = Appearance.getColorScheme() === 'dark' ? darkColors : lightColors;
